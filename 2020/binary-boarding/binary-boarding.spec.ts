d1eab839589aa5b0e32f8f4c79f583a95843d83a
cah-jeffeinhaus <jeffrey.einhaus@cardinalhealth.com>
2020-12-05 08:56:26 -0500

    Solution for binary boarding

---
+++ 2020/binary-boarding/binary-boarding.spec.ts
@@ -0,0 +1,29 @@
+import { computeSeatId, maxSeatId, findMySeat } from './binary-boarding'
+import { fileToStringArray } from '@utils/file-reader';
+
+describe('Binary Boarding', () => {
+  
+  let input: string[] = [];
+  beforeAll(() => {
+    input = fileToStringArray(__dirname, 'puzzle_input.txt');
+  });
+
+  it('computes the seat id by finding the row, multiplying by 8, then adding the column', () => {
+    expect(computeSeatId('FBFBBFFRLR')).toEqual(357);
+    expect(computeSeatId('BFFFBBFRRR')).toEqual(567);
+    expect(computeSeatId('FFFBBBFRRR')).toEqual(119);
+    expect(computeSeatId('BBFFBBFRLL')).toEqual(820);
+  });
+
+  it('computes the highest seat ID for the puzzle input', () => {
+    const result = maxSeatId(input);
+    console.log(result);
+    expect(result).not.toEqual(0);
+  });
+
+  it('finds my seat for the puzzle input', () => {
+    const result = findMySeat(input);
+    console.log(result);
+    expect(result).not.toEqual(0);
+  });
+});
---
+++ 2020/binary-boarding/binary-boarding.ts
@@ -0,0 +1,42 @@
+export const maxSeatId = (boardingPasses: string[]): number => {
+  return Math.max(...boardingPasses.map((boardingPass) => computeSeatId(boardingPass)));
+}
+
+export const findMySeat = (boardingPasses: string[]): number => {
+  const seats = boardingPasses.map((boardingPass) => computeSeatId(boardingPass))
+    .sort((a, b) => (a - b));
+
+  for (let i = 0; i < seats.length - 1; i++) {
+    if (seats[i + 1] - seats[i] == 2) {
+      return seats[i] + 1;
+    }
+  }
+  return 0;
+}
+
+export const computeSeatId = (boardingPass: string): number => {
+  const [row, column] = findSeat(boardingPass);
+  
+  return row * 8 + column;
+}
+
+const findSeat = (boardingPass: string): [number, number] => {
+  const rows = boardingPass.substring(0, 7);
+  const columns = boardingPass.substring(7);
+  const row = binarySearch(rows, 'F', 'B', 0, 127);
+  const column = binarySearch(columns, 'L', 'R', 0, 7);
+
+  return [row, column];
+}
+
+const binarySearch = (partitions: string, minRegion: string, maxRegion: string, minBound: number, maxBound: number): number => {
+  [...partitions].forEach((binarySpacePartition: string) => {
+    if (binarySpacePartition === minRegion) {
+      maxBound -= Math.ceil((maxBound - minBound) / 2);
+    } else if (binarySpacePartition === maxRegion) {
+      minBound += Math.ceil((maxBound - minBound) / 2);
+    }
+  });
+
+  return minBound;
+};
