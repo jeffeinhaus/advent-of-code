aa6799675efe3c27bbcd6450e2324e11df9d1da7
cah-jeffeinhaus <jeffrey.einhaus@cardinalhealth.com>
2020-12-26 10:27:57 -0500

    Day 4 2015, The Ideal Stocking Stuffer - Refactor to extract hash function

--- 2015/the-ideal-stocking-stuffer/index.ts
+++ 2015/the-ideal-stocking-stuffer/index.ts
@@ -2,11 +2,8 @@ import { createHash } from 'crypto';
 
 export const findSmallestHashWithNLeadingZeroes = (leadingZeroes: number, key: string): number => {
   let count = -1;
-  let hash = '';
-  while (!hash.startsWith('0'.repeat(leadingZeroes))) {
-    hash = createHash('md5')
-      .update(`${key}${++count}`)
-      .digest('hex');
-  }
+  while (!computeHash(`${key}${++count}`).startsWith('0'.repeat(leadingZeroes))) {}
   return count;
 };
+
+const computeHash = (toHash: string): string => createHash('md5').update(toHash).digest('hex');
