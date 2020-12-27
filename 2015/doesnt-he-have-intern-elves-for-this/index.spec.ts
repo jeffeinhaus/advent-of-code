2cce3340ab60e7515a075e74e0c924e42d2d34c6
cah-jeffeinhaus <jeffrey.einhaus@cardinalhealth.com>
2020-12-27 17:42:32 -0500

    Day 5 2015, Doesn't He Have Intern-Elves For This? - Part 2

--- 2015/doesnt-he-have-intern-elves-for-this/index.spec.ts
+++ 2015/doesnt-he-have-intern-elves-for-this/index.spec.ts
@@ -1,73 +1,125 @@
 import {
   containsAtLeastThreeVowels,
   containsDoubleLetter,
+  containsNonOverlappingPair,
+  containsRepeatedWithCharacterBetween,
   containsSpecialSequences,
+  countNicePart2Strings,
   countNiceStrings,
   isNice,
+  isNicePart2,
 } from '.';
 import { fileToStringArray } from '@utils/file-reader';
 
 describe("Doesn't He Have Intern-Elves For This?", () => {
   const puzzle = fileToStringArray(__dirname, 'input/puzzle.txt');
 
-  describe(containsAtLeastThreeVowels, () => {
-    it('returns true if the string contains at least 3 vowels', () => {
-      expect(containsAtLeastThreeVowels('aei')).toEqual(true);
-      expect(containsAtLeastThreeVowels('xazegov')).toEqual(true);
-      expect(containsAtLeastThreeVowels('aeiouaeiouaeiou')).toEqual(true);
+  describe('Part 1', () => {
+    describe(containsAtLeastThreeVowels, () => {
+      it('returns true if the string contains at least 3 vowels', () => {
+        expect(containsAtLeastThreeVowels('aei')).toEqual(true);
+        expect(containsAtLeastThreeVowels('xazegov')).toEqual(true);
+        expect(containsAtLeastThreeVowels('aeiouaeiouaeiou')).toEqual(true);
+      });
+
+      it('returns false if the string contains less than 3 vowels', () => {
+        expect(containsAtLeastThreeVowels('aey')).toEqual(false);
+        expect(containsAtLeastThreeVowels('xazegyv')).toEqual(false);
+        expect(containsAtLeastThreeVowels('qwrtyqwrtyqwrty')).toEqual(false);
+      });
     });
 
-    it('returns false if the string contains less than 3 vowels', () => {
-      expect(containsAtLeastThreeVowels('aey')).toEqual(false);
-      expect(containsAtLeastThreeVowels('xazegyv')).toEqual(false);
-      expect(containsAtLeastThreeVowels('qwrtyqwrtyqwrty')).toEqual(false);
+    describe(containsDoubleLetter, () => {
+      it('returns true if the string contains a letter that appears twice in a row', () => {
+        expect(containsDoubleLetter('xx')).toEqual(true);
+        expect(containsDoubleLetter('abcdde')).toEqual(true);
+        expect(containsDoubleLetter('aabbccdd')).toEqual(true);
+      });
+
+      it('returns false if the string does not contain a letter that appears twice in a row', () => {
+        expect(containsDoubleLetter('xy')).toEqual(false);
+        expect(containsDoubleLetter('abcdef')).toEqual(false);
+        expect(containsDoubleLetter('ababcdcd')).toEqual(false);
+      });
+    });
+
+    describe(containsSpecialSequences, () => {
+      it('returns true if the string contains ab, cd, pq, or xy', () => {
+        expect(containsSpecialSequences('ab')).toEqual(true);
+        expect(containsSpecialSequences('abcdde')).toEqual(true);
+        expect(containsSpecialSequences('ababpqxyde')).toEqual(true);
+      });
+
+      it('returns false if the string does not contain ab, cd, pq, or xy', () => {
+        expect(containsSpecialSequences('bc')).toEqual(false);
+        expect(containsSpecialSequences('bdde')).toEqual(false);
+        expect(containsSpecialSequences('bcbcpnoede')).toEqual(false);
+      });
+    });
+
+    describe(isNice, () => {
+      it('returns true if it has at least 3 vowels, contains a double letter, and does not contain a special sequence', () => {
+        expect(isNice('ugknbfddgicrmopn')).toEqual(true);
+        expect(isNice('aaa')).toEqual(true);
+      });
+
+      it('returns false if it does not meet the nice criteria', () => {
+        expect(isNice('jchzalrnumimnmhp')).toEqual(false);
+        expect(isNice('haegwjzuvuyypxyu')).toEqual(false);
+        expect(isNice('dvszwmarrgswjxmb')).toEqual(false);
+      });
+    });
+
+    describe(countNiceStrings, () => {
+      it('counts the number of nice strings', () => {
+        expect(countNiceStrings(puzzle)).toEqual(238);
+      });
     });
   });
 
-  describe(containsDoubleLetter, () => {
-    it('returns true if the string contains a letter that appears twice in a row', () => {
-      expect(containsDoubleLetter('xx')).toEqual(true);
-      expect(containsDoubleLetter('abcdde')).toEqual(true);
-      expect(containsDoubleLetter('aabbccdd')).toEqual(true);
+  describe('Part 2', () => {
+    describe(containsNonOverlappingPair, () => {
+      it('returns true if the string contains at least one duplicate pair that does not overlap', () => {
+        expect(containsNonOverlappingPair('xyxy')).toEqual(true);
+        expect(containsNonOverlappingPair('aabcdefgaa')).toEqual(true);
+      });
+
+      it('returns false if the string does not contain a duplicate or if the duplicate overlaps', () => {
+        expect(containsNonOverlappingPair('aaa')).toEqual(false);
+        expect(containsNonOverlappingPair('mnbvcaaaqwer')).toEqual(false);
+      });
     });
 
-    it('returns false if the string does not contain a letter that appears twice in a row', () => {
-      expect(containsDoubleLetter('xy')).toEqual(false);
-      expect(containsDoubleLetter('abcdef')).toEqual(false);
-      expect(containsDoubleLetter('ababcdcd')).toEqual(false);
-    });
-  });
+    describe(containsRepeatedWithCharacterBetween, () => {
+      it('returns true if the string contains a letter that repeats with exactly one letter between', () => {
+        expect(containsRepeatedWithCharacterBetween('xyx')).toEqual(true);
+        expect(containsRepeatedWithCharacterBetween('abcdefeghi')).toEqual(true);
+        expect(containsRepeatedWithCharacterBetween('aaa')).toEqual(true);
+      });
 
-  describe(containsSpecialSequences, () => {
-    it('returns true if the string contains ab, cd, pq, or xy', () => {
-      expect(containsSpecialSequences('ab')).toEqual(true);
-      expect(containsSpecialSequences('abcdde')).toEqual(true);
-      expect(containsSpecialSequences('ababpqxyde')).toEqual(true);
-    });
-
-    it('returns false if the string does not contain ab, cd, pq, or xy', () => {
-      expect(containsSpecialSequences('bc')).toEqual(false);
-      expect(containsSpecialSequences('bdde')).toEqual(false);
-      expect(containsSpecialSequences('bcbcpnoede')).toEqual(false);
-    });
-  });
-
-  describe(isNice, () => {
-    it('returns true if it has at least 3 vowels, contains a double letter, and does not contain a special sequence', () => {
-      expect(isNice('ugknbfddgicrmopn')).toEqual(true);
-      expect(isNice('aaa')).toEqual(true);
+      it('returns false if the string does not contains a letter that repeats with exactly one letter between', () => {
+        expect(containsRepeatedWithCharacterBetween('xyz')).toEqual(false);
+        expect(containsRepeatedWithCharacterBetween('abcdefghi')).toEqual(false);
+        expect(containsRepeatedWithCharacterBetween('aab')).toEqual(false);
+      });
     });
 
-    it('returns false if it does not meet the nice criteria', () => {
-      expect(isNice('jchzalrnumimnmhp')).toEqual(false);
-      expect(isNice('haegwjzuvuyypxyu')).toEqual(false);
-      expect(isNice('dvszwmarrgswjxmb')).toEqual(false);
-    });
-  });
+    describe(isNicePart2, () => {
+      it('returns true if it has a non-overlapping duplicate, and a letter that repeats with one between', () => {
+        expect(isNicePart2('qjhvhtzxzqqjkmpb')).toEqual(true);
+        expect(isNicePart2('xxyxx')).toEqual(true);
+      });
 
-  describe(countNiceStrings, () => {
-    it('counts the number of nice strings', () => {
-      expect(countNiceStrings(puzzle)).toEqual(238);
+      it('returns false if it does not have a non-overlapping duplicate, and a letter that repeats with one between', () => {
+        expect(isNicePart2('uurcxstgmygtbstg')).toEqual(false);
+        expect(isNicePart2('ieodomkazucvgmuy')).toEqual(false);
+      });
+    });
+
+    describe(countNicePart2Strings, () => {
+      it('counts the number of nice strings', () => {
+        expect(countNicePart2Strings(puzzle)).toEqual(69);
+      });
     });
   });
 });
--- 2015/doesnt-he-have-intern-elves-for-this/index.ts
+++ 2015/doesnt-he-have-intern-elves-for-this/index.ts
@@ -9,10 +9,38 @@ export const containsDoubleLetter = (toCheck: string): boolean =>
 export const containsSpecialSequences = (toCheck: string): boolean =>
   RegExp(/ab|cd|pq|xy/).test(toCheck);
 
+export const containsNonOverlappingPair = (toCheck: string): boolean => {
+  for (let i = 0; i < toCheck.length - 1; i++) {
+    const pair = toCheck.substring(i, i + 2);
+    for (let j = i + 2; j < toCheck.length - 1; j++) {
+      if (pair === toCheck.substring(j, j + 2)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+export const containsRepeatedWithCharacterBetween = (toCheck: string): boolean => {
+  for (let i = 0; i < toCheck.length - 2; i++) {
+    if (toCheck.charAt(i) === toCheck.charAt(i + 2)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 export const isNice = (toCheck: string): boolean =>
   containsAtLeastThreeVowels(toCheck) &&
   containsDoubleLetter(toCheck) &&
   !containsSpecialSequences(toCheck);
 
-export const countNiceStrings = (strings: string[]): number =>
-  strings.filter((toCheck) => isNice(toCheck)).length;
+export const isNicePart2 = (toCheck: string): boolean =>
+  containsNonOverlappingPair(toCheck) && containsRepeatedWithCharacterBetween(toCheck);
+
+export const countNiceStrings = (strings: string[]): number => solver(strings, isNice);
+
+export const countNicePart2Strings = (strings: string[]): number => solver(strings, isNicePart2);
+
+const solver = (strings: string[], niceFunction: (toCheck: string) => boolean): number =>
+  strings.filter(niceFunction).length;
