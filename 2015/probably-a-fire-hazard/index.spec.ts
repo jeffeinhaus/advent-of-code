f7398b35ad3ab45b1b3db9bdfaa61e505d0a035c
cah-jeffeinhaus <jeffrey.einhaus@cardinalhealth.com>
2020-12-29 10:59:39 -0500

    Day 6 2015, Probably A Fire Hazard - Part 2

--- 2015/probably-a-fire-hazard/index.spec.ts
+++ 2015/probably-a-fire-hazard/index.spec.ts
@@ -1,4 +1,10 @@
-import { InstructionType, LightGrid, convertToInstruction, countOnLights } from '.';
+import {
+  InstructionType,
+  LightGrid,
+  convertToInstruction,
+  countOnLights,
+  getTotalBrightness,
+} from '.';
 import { fileToStringArray } from '@utils/file-reader';
 
 describe('Probably a Fire Hazard', () => {
@@ -11,12 +17,23 @@ describe('Probably a Fire Hazard', () => {
     });
 
     describe('turnOn', () => {
-      it('switches the lights to true in the range', () => {
+      beforeEach(() => {
         grid.turnOn({ x: 0, y: 0 }, { x: 999, y: 999 });
+      });
+
+      it('switches the lights to true in the range', () => {
         expect(
-          grid.lights[Math.floor(Math.random() * 999) + 1][Math.floor(Math.random() * 999) + 1],
+          grid.lights[Math.floor(Math.random() * 999) + 1][Math.floor(Math.random() * 999) + 1].on,
         ).toEqual(true);
         expect(grid.getLightsOn()).toEqual(1000000);
+      });
+
+      it('increases the brightness by 1 in the range', () => {
+        expect(
+          grid.lights[Math.floor(Math.random() * 999) + 1][Math.floor(Math.random() * 999) + 1]
+            .brightness,
+        ).toEqual(1);
+        expect(grid.getBrightness()).toEqual(1000000);
       });
     });
 
@@ -24,9 +41,16 @@ describe('Probably a Fire Hazard', () => {
       it('toggles the lights in the range', () => {
         grid.turnOn({ x: 500, y: 0 }, { x: 999, y: 0 });
         grid.toggle({ x: 0, y: 0 }, { x: 999, y: 0 });
-        expect(grid.lights[Math.floor(Math.random() * 500) + 1][0]).toEqual(true);
-        expect(grid.lights[Math.floor(Math.random() * 500) + 499][0]).toEqual(false);
+        expect(grid.lights[Math.floor(Math.random() * 500) + 1][0].on).toEqual(true);
+        expect(grid.lights[Math.floor(Math.random() * 500) + 499][0].on).toEqual(false);
         expect(grid.getLightsOn()).toEqual(500);
+      });
+
+      it('increases the brightness by 2 in the range', () => {
+        grid.toggle({ x: 0, y: 0 }, { x: 999, y: 0 });
+        expect(grid.lights[Math.floor(Math.random() * 500) + 1][0].brightness).toEqual(2);
+        expect(grid.lights[Math.floor(Math.random() * 500) + 499][0].brightness).toEqual(2);
+        expect(grid.getBrightness()).toEqual(2000);
       });
     });
 
@@ -34,10 +58,21 @@ describe('Probably a Fire Hazard', () => {
       it('switches the lights to false in the range', () => {
         grid.turnOn({ x: 499, y: 499 }, { x: 500, y: 500 });
         grid.turnOff({ x: 499, y: 499 }, { x: 500, y: 500 });
-        expect(grid.lights[499][499]).toEqual(false);
-        expect(grid.lights[499][500]).toEqual(false);
-        expect(grid.lights[500][499]).toEqual(false);
-        expect(grid.lights[500][500]).toEqual(false);
+        expect(grid.lights[499][499].on).toEqual(false);
+        expect(grid.lights[499][500].on).toEqual(false);
+        expect(grid.lights[500][499].on).toEqual(false);
+        expect(grid.lights[500][500].on).toEqual(false);
+      });
+
+      it('decreases the brightness by 1 in the range', () => {
+        grid.turnOn({ x: 499, y: 499 }, { x: 500, y: 500 });
+        grid.turnOn({ x: 499, y: 499 }, { x: 500, y: 500 });
+        grid.turnOff({ x: 499, y: 499 }, { x: 500, y: 500 });
+        expect(grid.lights[499][499].brightness).toEqual(1);
+        expect(grid.lights[499][500].brightness).toEqual(1);
+        expect(grid.lights[500][499].brightness).toEqual(1);
+        expect(grid.lights[500][500].brightness).toEqual(1);
+        expect(grid.getBrightness()).toEqual(4);
       });
     });
   });
@@ -65,6 +100,12 @@ describe('Probably a Fire Hazard', () => {
   describe(countOnLights, () => {
     it('counts the number of turned on lights after executing all the instructions', () => {
       expect(countOnLights(puzzle)).toEqual(569999);
+    });
+  });
+
+  describe(getTotalBrightness, () => {
+    it('counts the total brightness after executing all the instructions', () => {
+      expect(getTotalBrightness(puzzle)).toEqual(17836115);
     });
   });
 });
--- 2015/probably-a-fire-hazard/index.ts
+++ 2015/probably-a-fire-hazard/index.ts
@@ -1,10 +1,15 @@
 import { Position } from '@types';
 
+type Light = {
+  on: boolean;
+  brightness: number;
+};
+
 export class LightGrid {
-  public readonly lights: boolean[][];
+  public readonly lights: Light[][];
 
   constructor(gridSize: number) {
-    this.lights = [...Array(gridSize)].map(() => Array(gridSize).fill(false));
+    this.lights = this.initialize2dLightsArray(gridSize);
   }
 
   public turnOn = (start: Position, end: Position): void => {
@@ -18,7 +23,8 @@ export class LightGrid {
   public toggle = (start: Position, end: Position): void => {
     for (let i = start.x; i <= end.x; i++) {
       for (let j = start.y; j <= end.y; j++) {
-        this.lights[i][j] = !this.lights[i][j];
+        this.lights[i][j].on = !this.lights[i][j].on;
+        this.lights[i][j].brightness += 2;
       }
     }
   };
@@ -27,18 +33,42 @@ export class LightGrid {
     let lightsOn = 0;
     for (let i = 0; i < this.lights.length; i++) {
       for (let j = 0; j < this.lights[i].length; j++) {
-        lightsOn = this.lights[i][j] ? lightsOn + 1 : lightsOn;
+        lightsOn = this.lights[i][j].on ? lightsOn + 1 : lightsOn;
       }
     }
     return lightsOn;
   };
 
+  public getBrightness = (): number => {
+    let brightness = 0;
+    for (let i = 0; i < this.lights.length; i++) {
+      for (let j = 0; j < this.lights[i].length; j++) {
+        brightness += this.lights[i][j].brightness;
+      }
+    }
+    return brightness;
+  };
+
   private switchInRange = (start: Position, end: Position, shouldTurnOn: boolean): void => {
     for (let i = start.x; i <= end.x; i++) {
       for (let j = start.y; j <= end.y; j++) {
-        this.lights[i][j] = shouldTurnOn;
+        this.lights[i][j].on = shouldTurnOn;
+        this.lights[i][j].brightness = shouldTurnOn
+          ? this.lights[i][j].brightness + 1
+          : Math.max(this.lights[i][j].brightness - 1, 0);
       }
     }
+  };
+
+  private initialize2dLightsArray = (gridSize: number): Light[][] => {
+    const lightsArray = new Array(gridSize);
+    for (let i = 0; i < gridSize; i++) {
+      lightsArray[i] = new Array(gridSize);
+      for (let j = 0; j < gridSize; j++) {
+        lightsArray[i][j] = { on: false, brightness: 0 };
+      }
+    }
+    return lightsArray;
   };
 }
 
@@ -75,7 +105,13 @@ export const convertToInstruction = (instruction: string): Instruction => {
   };
 };
 
-export const countOnLights = (instructions: string[]): number => {
+export const countOnLights = (instructions: string[]): number =>
+  executeInstructions(instructions).getLightsOn();
+
+export const getTotalBrightness = (instructions: string[]): number =>
+  executeInstructions(instructions).getBrightness();
+
+const executeInstructions = (instructions: string[]): LightGrid => {
   const grid = new LightGrid(1000);
 
   instructions
@@ -90,5 +126,5 @@ export const countOnLights = (instructions: string[]): number => {
       }
     });
 
-  return grid.getLightsOn();
+  return grid;
 };
